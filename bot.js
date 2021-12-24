4f39204202f4631d94692dae1b8d79abc4b2eb36
Heitz Remi <remi.heitz@student-cs.fr>
2021-12-24 18:08:36 +0100

    Added default values

--- bot.js
+++ bot.js
@@ -9,15 +9,18 @@ const client = new Discord.Client({ intents });
 
 var channelId;
 var currentPollId;
+var servers;
 
 if (process.env.run_mode == "prod"){
 	channelId = config.prod[0].channelId;
 	serverId = config.prod[0].serverId;
+	servers = config.prod;
 	console.log("⚠️ Starting in production mode ⚠️");
 }
 else {
 	channelId = config.dev.channelId;
 	serverId = config.dev.serverId;
+	servers = [config.dev];
 	if (process.env.run_mode !== "dev"){
 		console.log("Incorrect value for 'run_mode' in the config file : defaulting to development mode");
 	}
@@ -27,18 +30,22 @@ else {
 var createdAt;
 var currentPollId;
 
-const createPoll = async (pingPlayers) => {
-	const channel = await client.channels.cache.get(channelId);
+const addStringTime = (date, timeString) => {
+	const matches = timeString.match(/(\d+)h(\d+)?/);
+	return new Date(date.getTime() + ((matches[0] || 0)*60 + (matches[1] || 0))*60*1000)
+}
+
+const createPoll = async (pingPlayers, targetChannelId, color, title, desc, duration, number) => {
+	const channel = await client.channels.cache.get(targetChannelId);
 	channel.bulkDelete(99);
 	createdAt = new Date();
-	expiration = new Date(createdAt.getTime() + 4*60*60*1000);
+	expiration = addStringTime(createdAt, duration || config.defaults.duration);
 	const embed = new MessageEmbed()
-		.setColor("#256579")
-		.setTitle("Nouveau 5v5 !")
-		.setDescription('Ajoutez une réaction pour vous déclarer comme volontaire pour un 5v5 !\
-		Quand 10 personnes se seront portées volontaires, vous serez ping pour venir jouer')
-		.addField('Poll Duration', '4h', true)
-		.addField('Required number', '10', true)
+		.setColor(color || config.defaults.color)
+		.setTitle(title || config.defaults.title)
+		.setDescription(desc || config.defaults.description)
+		.addField('Poll Duration', duration || config.defaults.duration, true)
+		.addField('Required number', number || config.defaults.number, true)
 		.addField('Creation', createdAt.toLocaleString(), true)
 		.addField('Expiration', expiration.toLocaleString(), true);
 	const message = await channel.send({embeds: [embed]});
@@ -48,14 +55,15 @@ const createPoll = async (pingPlayers) => {
 
 client.once("ready",async () => {
 	console.log("5v5 bot online");
-	await createPoll(false);
+	//await createPoll(false);
+	await createPoll(false, servers[0].channelId);
 });
 
 client.on('messageReactionAdd', async (reaction,user) =>{
 	if (user.bot || reaction.message.channelId !== channelId || reaction.message.id !== currentPollId) return;
 	let now = new Date();
 	if (now - createdAt > 4*60*60*1000){
-		createPoll(false);
+		createPoll(false, servers[0].channelId);
 	}
 	const count = reaction.count - 1;
 	console.log(`Count : ${count}`);
@@ -76,7 +84,7 @@ client.on("messageCreate", async (message) => {
 
 	if (message.author.bot || message.channelId !== channelId) return;
 	if (message.content === "new"){
-		await createPoll(true);
+		await createPoll(true, servers[0].channelId);
 	}
 	else{
 		message.delete();
